2ca684ea415f3d15fc5daa817b2f148fb225d01d
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-11-23 13:16:49 -0600

    root index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider } from "./context/Modal";
+import App from "./App";
+
+import configureStore from "./store";
+import { restoreCSRF, fetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+// const Carrot = () => (
+//   <div style={{ color: "orange", fontSize: "100px" }}>
+//     <i className="fas fa-carrot"></i>
+//   </div>
+// );
+
+function Root() {
+  return (
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          {/* <Carrot /> */}
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  //   <React.StrictMode>
+  <Root />,
+  //   </React.StrictMode>,
+  document.getElementById("root")
 );
