5e6d55703b75907bc397243aad808deb7d4a1bbd
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-11-25 10:40:35 -0600

    backend structuring

--- backend/db/migrations/20201123181453-create-task.js
+++ backend/db/migrations/20201123181453-create-task.js
@@ -9,24 +9,31 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       description: {
+        allowNull: false,
         type: Sequelize.TEXT
       },
       deadline: {
+        allowNull: false,
         type: Sequelize.DATE
       },
       projectId: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       creatorId: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       completed: {
+        allowNull: false,
         type: Sequelize.BOOLEAN
       },
       assignedtoId: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       createdAt: {
